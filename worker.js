589429cb4ac10d53f63b79741f22cb43ddd55d71
Athul M S <athulms@gmail.com>
2018-11-15 17:17:10 +0530

    Adding the click functionality on web push notification click

--- worker.js
+++ worker.js
@@ -1,10 +1,32 @@
 console.log('Loaded service worker!');
-
+self.addEventListener('notificationclick', function(event) {
+  let url = event.notification.data.url;
+  event.notification.close(); // Android needs explicit close.
+  event.waitUntil(
+      clients.matchAll({type: 'window'}).then( windowClients => {
+          // Check if there is already a window/tab open with the target URL
+          for (var i = 0; i < windowClients.length; i++) {
+              var client = windowClients[i];
+              // If so, just focus it.
+              if (client.url === url && 'focus' in client) {
+                  return client.focus();
+              }
+          }
+          // If not, then open the target URL in a new window/tab.
+          if (clients.openWindow) {
+              return clients.openWindow(url);
+          }
+      })
+  );
+});
 self.addEventListener('push', ev => {
   const data = ev.data.json();
   console.log('Got push', data);
   self.registration.showNotification(data.title, {
     body: data.body,
+    data: {
+      url: data.url
+    },
     icon: "https://www.carrentals.com/favicon.ico"
   });
 });
